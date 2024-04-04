bf4b6ddd9a8a865a1b82a20fd8da613030a5636f
AS-Coope <deltazirconium@gmail.com>
2024-04-04 16:23:28 -0500

    Setup preliminary structure of Movies Database

--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
-# Add any model classes for Flask-SQLAlchemy here
+from . import db
+
+class Movies(db.Model):
+
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    poster = db.Column(db.String(200))
+    date_created = db.Column(db.DateTime)
