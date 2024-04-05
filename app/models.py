23096f305106fe719fd83665c0877e93c706842b
AS-Coope <deltazirconium@gmail.com>
2024-04-05 15:43:45 -0500

    Setup init method for Movies Model

--- app/models.py
+++ app/models.py
@@ -8,3 +8,9 @@ class Movies(db.Model):
     description = db.Column(db.Text)
     poster = db.Column(db.String(200))
     date_created = db.Column(db.DateTime)
+
+    def __init__(self, title, description, poster, date_created):
+        self.title = title
+        self.description = description
+        self.poster = poster
+        self.date_created = date_created
