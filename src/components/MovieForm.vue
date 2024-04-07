6965a6dd806d222dbf1bbb3b6f4f189b8fb6fc6a
AS-Coope <deltazirconium@gmail.com>
2024-04-07 08:29:27 -0500

    Create MovieForm vue file

---
+++ src/components/MovieForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <form @submit.prevent="saveMovie">
+        <div class="form-group mb-3">
+            <label for="title" class="form-label">
+                Movie Title
+            </label>
+            <input type="text" name="title" class="form-control">
+            <label for="description" class="form-label">
+                Movie Description
+            </label>
+            <input type="textarea" name="description" class="form-control">
+        </div>
+    </form>
+</template>
+
+<script setup>
+//const props = defineProps(['movie'])
+//const emit = defineEmits(['edit', 'remove'])
+
+function saveMovie() {
+    fetch("/api/v1/movies", { 
+        method: 'POST' 
+    })
+    .then(response => { 
+            return response.json(); 
+    }) 
+    .then(data => { 
+        // display a success message 
+        console.log(data); 
+    }) 
+    .catch(error => { 
+        console.log(error); 
+    })
+}
+
+function removeMovie() {
+    emit('remove')
+}
+</script>
